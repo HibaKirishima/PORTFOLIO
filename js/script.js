a89c20594873ab95f8fe63053c0b86e196d4ad27
HibaKirishima <153572447+HibaKirishima@users.noreply.github.com>
2024-12-09 15:36:28 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Function to trigger the animation on the image
+function startAnimation() {
+    const image = document.getElementById("resume");
+    image.classList.add("animate"); // Add the class to start the animation
+}
